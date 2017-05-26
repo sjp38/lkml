4c72f30825f579e90947887a15f51911fb7517b6
SeongJae Park <sj38.park@gmail.com>
2017-05-26 09:30:27 +0900

    Implement simple rss fetching function
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- lkmllive.go
+++ lkmllive.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("Hello\n")
+	resp, err := http.Get("https://lkml.org/rss.php")
+	if err != nil {
+		panic(fmt.Sprintf("failed to get rss: %s", err))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		panic(fmt.Sprintf("failed to read body: %s", err))
+	}
+
+	fmt.Printf(string(body))
 }
