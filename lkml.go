ca92ad44f9e7324ce8996ce7272ccf5de0f7de97
SeongJae Park <sj38.park@gmail.com>
2017-05-26 10:40:10 +0900

    Print author and link info, too
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- lkml.go
+++ lkml.go
@@ -61,7 +61,6 @@ func main() {
 
 	// 0th index is rss channel title. So, skip it.
 	for i := len(items) - 1; i > 0; i-- {
-		fmt.Printf("%s\n", items[i].title)
-		fmt.Printf("%s\n", items[i].link)
+		fmt.Printf("%s\n\t%s\n\t%s\n", items[i].title, items[i].author, items[i].link)
 	}
 }
